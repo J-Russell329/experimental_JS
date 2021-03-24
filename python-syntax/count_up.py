632ec546d8a05b6e82cce55cbdd84ba660336460
J-Russell329 <jtr.russell329@gmail.com>
2021-03-24 12:57:12 -0500

    finished the python section 1 exercise

---
+++ python-syntax/count_up.py
@@ -0,0 +1,21 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+    if start < stop:
+        while start <= stop:
+            print(start)
+            start += 1
+    else:
+        print("stoping number must be greater than starting number")
+
+count_up(5, 7)        
---
+++ python-syntax/in_range.py
@@ -0,0 +1,28 @@
+def in_range(nums, lowest, highest):
+	"""Print numbers inside range.
+
+    - nums: list of numbers
+    - lowest: lowest number to print
+    - highest: highest number to print
+
+    For example:
+
+      in_range([10, 20, 30, 40], 15, 30)
+
+    should print:
+
+      20 fits
+      30 fits
+    """
+	new_range = []
+	for num in nums:
+		if num > lowest and num <= highest:
+			new_range.append(num)
+	return new_range
+
+	
+
+    
+
+
+in_range([10, 20, 30, 40, 50], 15, 30)            
