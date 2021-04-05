bf271949df21d25149e094e6d59f44d401c262c8
J-Russell329 <jtr.russell329@gmail.com>
2021-04-05 18:00:34 -0500

    finsihed the python creating a server exercise

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,31 @@
-# Put your app in here.
+from flask import Flask, request 
+app = Flask(__name__)
+
+@app.route('/<opperation>')
+def math(opperation):
+    num1 = int(request.args.get("a"))
+    num2 = int(request.args.get("b"))
+    if opperation == 'add':
+        return  str(num1+num2)
+    elif opperation == 'sub':
+        return str(num1 - num2)
+    elif opperation == 'mult':
+        return str(num1 * num2)
+    elif opperation == 'div':
+        return str(num1 / num2)
+
+
+@app.route('/math/<opperation>')
+def more_math(opperation):
+    """ duplicated under just so that the test would not give any errors"""
+    num1 = int(request.args.get("a"))
+    num2 = int(request.args.get("b"))
+    if opperation == 'add':
+        return  str(num1+num2)
+    elif opperation == 'sub':
+        return str(num1 - num2)
+    elif opperation == 'mult':
+        return str(num1 * num2)
+    elif opperation == 'div':
+        return str(num1 / num2)
+
