3617bfa7b258b63f55b9de934f2be8975b19965e
J-Russell329 <jtr.russell329@gmail.com>
2021-04-14 12:21:49 -0500

    upgraded flask-survey-2 to have session funcationality before sending data off as one list

--- flask-survey-2/app.py
+++ flask-survey-2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request, redirect, url_for, flash
+from flask import Flask , render_template, request, redirect, url_for, flash, session
 from surveys import * 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,9 +10,11 @@ debug = DebugToolbarExtension(app)
 
 answers = []
 
-@app.route('/')
+
+@app.route('/',)
 def home():
     return render_template('home.html')
+
 
 @app.route('/question/<int:question_number>')
 def question(question_number):
@@ -33,6 +35,7 @@ def question(question_number):
             flash('stop trying to do questions out of order')
             return redirect(f'/question/{len(answers)}')
 
+
 @app.route('/question/send')
 def send_data():
     temp_answer = list(request.args)
@@ -40,5 +43,39 @@ def send_data():
         answers.append(temp_answer[0])
     return redirect(f'/question/{len(answers)}')
 
-@app.route('session', methods=['POST'])
-def 
+
+@app.route('/session-questions/post', methods=['POST'])
+def posting():
+    temp_session = session['answers']
+    try:
+        temp_answer = list(request.form.values())[0]
+    except:
+        flash('Answer was not recorded. Try clicking your answer again')
+        return redirect((f'/session-question/{len(temp_session)}'))
+    temp_session.append(temp_answer)
+    session['answers'] = temp_session
+    return redirect((f'/session-question/{len(temp_session)}'))
+
+
+@app.route('/session-question/<int:question_number>')
+def session_question(question_number):
+    try:
+        question = satisfaction_survey.questions[question_number].question
+        choices = satisfaction_survey.questions[question_number].choices
+        if question_number == len(session['answers']):
+            return render_template('session-questions.html',question=question,choices=choices,next_number= question_number+1)
+        else:
+            return redirect(f"/session-question/{len(session['answers'])}")
+    except:
+        if question_number == len(session['answers']):
+            answers = session['answers']
+            return render_template('thank-you.html',answers=answers) 
+        else:
+            flash('stop trying to do questions out of order')
+            return redirect(f'/session-question/{len(answers)}')
+
+@app.route('/session-question/start')
+def session_questions_start():
+    session['answers'] = []
+    return redirect('/session-question/0')
+    
